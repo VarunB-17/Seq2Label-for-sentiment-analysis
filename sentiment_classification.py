f16941580cebc664d76f550d91abcdaef7f59ea4
Varun Behari <varun@hotmail.nl>
2023-04-10 18:36:52 +0200

    iteration over batches

--- sentiment_classification.py
+++ sentiment_classification.py
@@ -2,8 +2,11 @@ from dataloader import *
 
 
 data = DatasetSentiment(val=False)
-batches = DynamicBatchLoader(dataset=data[],
-                       max_tokens=MAX_TOKENS,
-                       batches=BATCHES)
+batches = DynamicBatchLoader(dataset=data,
+                             max_tokens=MAX_TOKENS,
+                             batch_size=BATCHES)
 
+for batch in batches:
 
+    for seq,label in batch:
+        print(seq.size())
