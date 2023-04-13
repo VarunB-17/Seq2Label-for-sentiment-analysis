124c4838129a153d541cfb74cf3f0a386634b88d
Varun Behari <varun@hotmail.nl>
2023-04-13 13:05:04 +0200

    + added explanations for each hyper-parameter

---
+++ params.py
@@ -0,0 +1,13 @@
+"""
+Hyper-parameters for binary classification of text
+where all values are some power of 2
+"""
+EPOCHS = 50 # amount of epochs
+PROGRESS = 10  # print progression of model training
+LR = 0.001  # learning rate
+MAX_TOKENS = 16348  # token limit of a batch
+EMBED_DIM = 128  # embedding size
+CLS = 2  # classes
+
+
+
