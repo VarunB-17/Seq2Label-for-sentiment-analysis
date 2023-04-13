6f56c5ed2684e1ba6a67af6e3996aea93591b5c0
Varun Behari <varun@hotmail.nl>
2023-04-13 13:01:17 +0200

    + changed batching process to fit padded sequences in memory instead of a batch going over the memory limit. + added documentation for each class and it's methods

--- dataloader.py
+++ dataloader.py
@@ -2,84 +2,10 @@
 Loads the IMDB dataset as a pandas dataframe
 in order to perform basic visualization.
 """
-import pandas as pd
-from data_rnn import load_imdb
 import torch
-from torch.utils.data import Dataset, DataLoader
-from torch.nn.utils.rnn import pad_sequence
-
-CLS, BATCHES, MAX_TOKENS = 2, 512, 64*2*2*2*2*2*2*2
-
-
-def data2df(val) -> tuple[pd.DataFrame, pd.DataFrame]:
-    """
-    Converts the raw data into a pandas dataframe,
-    and converts the list with integers into tensors.
-    """
-    # load data
-    (x_train, y_train), (x_test, y_test), (i2w, w2i), _ = load_imdb(final=val)
-
-    # Convert word ids to actual string representation of words
-    words_train = [[i2w[word] for word in sent] for sent in x_train]
-    words_test = [[i2w[word] for word in sent] for sent in x_test]
-
-    # Dataframe
-    df_train = pd.DataFrame({'sent': words_train,
-                             'x_train': x_train,
-                             'y_train': y_train})
-
-    df_test = pd.DataFrame({'sent': words_test,
-                            'x_test': x_test,
-                            'y_test': y_test})
-
-    # sorting data in ascending order
-    df_train['len'] = df_train['x_train'].apply(lambda x: len(x))
-    df_train = df_train.sort_values(by=['len'])
-
-    df_test['len'] = df_test['x_test'].apply(lambda x: len(x))
-    df_test = df_test.sort_values(by=['len'])
-
-    # convert list to tensor
-    df_train['x_train'] = df_train['x_train'].apply(lambda x: torch.tensor(x, dtype=torch.long))
-    df_train['y_train'] = df_train['y_train'].apply(lambda x: torch.tensor(x, dtype=torch.long))
-
-    df_test['x_test'] = df_test['x_test'].apply(lambda x: torch.tensor(x, dtype=torch.long))
-    df_test['y_test'] = df_test['y_test'].apply(lambda x: torch.tensor(x, dtype=torch.long))
-
-    return df_train, df_test
-
-
-def padding(batches):
-    """
-    pads batches according
-    to the longest sequence
-    in a batch and truncates
-    each batch until max_tokens
-    is reached.
-    """
-
-    padded_batches = []
-    for batch in batches:
-        sequences = [x[0] for x in batch]
-        labels = [x[1] for x in batch]
-        padded_seq = torch.nn.utils.rnn.pad_sequence(sequences, batch_first=True, padding_value=0)
-        padded_batches.append((padded_seq, torch.tensor(labels)))
-
-    return padded_batches
-
-
-def get_device():
-    """
-    Run on cpu
-    """
-    if torch.cuda.is_available():
-        device = torch.device('cuda:0')
-    else:
-        device = torch.device('cpu')
-
-    print(device)
-    return device
-
+from torch.utils.data import Dataset
+from params import MAX_TOKENS
+from batch_functions import formatted, padding, data2df, get_device, load_imdb
 
 class DatasetSentiment(Dataset):
     """
@@ -87,14 +13,15 @@ class DatasetSentiment(Dataset):
     the IMDB reviews dataset.
     """
 
-    def __init__(self, val=False):
-        self.val = val
-        self.df = data2df(val)
+    def __init__(self, final=False):
+        self.val = final
+        self.df = data2df(final)
         self.x_train = self.df[0]['x_train']
         self.y_train = self.df[0]['y_train']
         self.x_test = self.df[1]['x_test']
         self.y_test = self.df[1]['y_test']
 
+        # returns the size of the vocabulary
         vocab_dimension = torch.tensor([0])
         for tens in self.x_train.values:
             vocab_dimension = torch.cat((vocab_dimension, tens), dim=0)
@@ -110,36 +37,81 @@ class DatasetSentiment(Dataset):
         return len(self.x_train)
 
     def __getitem__(self, index):
+        """
+        returns a sequence and
+        label at some index -> [index]
+        """
         return self.x_train[index], self.y_train[index]
 
 
-class DynamicBatchLoader(DataLoader):
+class DynamicBatchLoader:
     """
-    Creates dynamic batches from
-    the DatasetSentiment class object.
+    Creates dynamic sizes batches where
+    each batch is filled with sequences
+    until some token limit is exceeded.
+    When the token limit is reached
+    it will move to a new batch
     """
 
-    def __init__(self, dataset, max_tokens, batch_size):
-        self.max_tokens = max_tokens
-        super().__init__(dataset, batch_size=batch_size, collate_fn=self.collate_fn, shuffle=False)
+    def __init__(self, dataset, max_tokens=MAX_TOKENS):
+        self.max_tokens = max_tokens # define maximum tokens allowed for each batch
+        self.dataset = dataset # original dataset
 
-    def collate_fn(self, batch):
-        batches = []
-        current_batch = []
-        current_tokens = 0
+    def dynamic(self):
+        """
+        returns batches according to the
+        method described in the class.
+        there are two cases when a batch
+        gets added, where in the first case
+        the addition of a sequence to a list
+        of padded sequences exceeds the token
+        limit, and a second case where if
+        a sequence without padding is under
+        the token limit but after padding
+        exceeds the limit.
+        This will allow for batches that will
+        never exceed the token limit and this
+        includes padded sequences.
+        """
 
-        for sequence, label in batch:
-            seq_len = len(sequence)
+        # Initiate empty batches
+        batches = list()
+        current_batch = list()
+        batch_length = int(0)
 
-            if seq_len + current_tokens > self.max_tokens:
-                batches.append(current_batch)
-                current_batch = []
-                current_tokens = 0
-            current_batch.append((sequence, label))
-            current_tokens += seq_len
+        # loop through the dataset
+        for i in range(len(self.dataset)):
+            (sequence, label) = self.dataset[i]
 
+            # get the length of the sequence
+            sequence_length = sequence.size()[0]
+
+            # case where without padding limit is exceeded
+            if batch_length + sequence_length > self.max_tokens:
+                x = [x[0] for x in current_batch]
+                print(f'{x[0].size()} {len(x)} - without padding')
+                batches.append(formatted(current_batch))
+                current_batch = list()
+
+            # temporary addition of a sequence only if including padded it still hold the max_token property
+            temp_seq = current_batch.copy()
+            temp_seq.append((sequence, label))
+            temp_batch, batch_length = padding(temp_seq)
+
+            # case where with padding limit is exceeded
+            if batch_length > self.max_tokens:
+                x = [x[0] for x in current_batch]
+                print(f'{x[0].size()} {len(x)} with padding')
+                batches.append(formatted(current_batch))
+                current_batch = list()
+                batch_length = int(0)
+            else:
+                current_batch = temp_batch
+
+        # add the last batch after iteration has been completed
         if current_batch:
-            batches.append(current_batch)
+            x = [x[0] for x in current_batch]
+            print(f'{x[0].size()} {len(x)} last batch')
+            batches.append(formatted(current_batch))
 
-        padded_batches = padding(batches)
-        return padded_batches
+        return batches
