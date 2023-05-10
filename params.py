3a2ea6bb079f3678c5dbe733a95c6635ceda6173
Varun Behari <varun@hotmail.nl>
2023-05-10 13:12:06 +0200

    updated params

--- params.py
+++ params.py
@@ -2,12 +2,17 @@
 Hyper-parameters for binary classification of text
 where all values are some power of 2
 """
-EPOCHS = 50 # amount of epochs
-PROGRESS = 10  # print progression of model training
+EPOCHS = 10  # amount of epochs
 LR = 0.001  # learning rate
 MAX_TOKENS = 8174  # token limit of a batch
-EMBED_DIM = 128  # embedding size
 CLS = 2  # classes
-
-
-
+VOCAB_SIZE = 99430  # vocabulary size
+EMBED_DIM = 512  # embedding dimension
+POOL = 'avg'  # type of pooling
+ATTENTION = 'mha'  # type of attention
+HEADS = 4  # number of attention heads
+DROPOUT = 0.0  # dropout rate
+HIDDEN = 2  # amount of hidden layers for ff
+ENCDEPTH = 4  # amount of encoder layers
+DECDEPTH = 4  # amount of decoder layer
+MASK = True
